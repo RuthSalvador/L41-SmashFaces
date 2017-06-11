f7bd2684cdda00b9bbf2e95b4b4fc3363a9bbda8
RuthSalvador <ruth.abby.sz@gmail.com>
2017-06-11 16:02:09 -0500

    puntos suma y resta ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var puntos = $('#puntos');
 var sumarPuntos = 0;
 var coder;
+var imgCoder;
+var url;
+var click = 0;
 
 var select = $('select');
 select.on('change', function () {
@@ -13,20 +16,30 @@ select.on('change', function () {
 
 function ElegirCoder(sede, nameSede) {
     coder = sede[Math.floor(Math.random() * sede.length)];
-    var imgCoder = coder.image;
-    var url = 'img/' + nameSede + '/' + imgCoder;
+    imgCoder = coder.image;
+    url = 'img/' + nameSede + '/' + imgCoder;
     $('img').attr('src', url);
 }
 
 var comprobar = $('#comprobar');
-comprobar.on('click', function () {
+comprobar.on('click', function (sede,nameSede) {
+    click += 1;
     var nameCoder = coder.name;
-    if ($('#name-input').val() == nameCoder){
+    if ((click < 5) && ($('#name-input').val().toLowerCase() == nameCoder.toLowerCase())){
+        alert('Excelente acertaste');
         sumarPuntos += 5;
         puntos.html('Puntos: '+ sumarPuntos + ' puntos');
-        
-    } else {
-        alert("Sigue intentando");
+        click = 0;
+       setInterval(ElegirCoder(sede,nameSede),2000);
+
+    } else if ((click < 5) && ($('#name-input').val().toLowerCase() != nameCoder.toLowerCase())){
+        puntos.html('Puntos: '+ sumarPuntos + ' puntos');
+        alert('Sigue intentando, te quedan '+ (5-click) + ' intentos');
+
+    } else if ((click >= 5) && ($('#name-input').val().toLowerCase() != nameCoder.toLowerCase())){
+        sumarPuntos -= 1;
+        puntos.html('Puntos: '+ sumarPuntos + ' puntos');
+        alert('Siguiente coder');
     }
 });
 
